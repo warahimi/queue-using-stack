c769d2abe177711e82670ee96cabe10f85027966
Wahidullah Rahimi <wahidrahimi45@gmail>
2023-07-28 12:10:30 -0700

    Queue Implementation Using single Stack

--- QueueUsingStack/QueueUsingStack/Program.cs
+++ QueueUsingStack/QueueUsingStack/Program.cs
@@ -6,23 +6,34 @@ namespace QueueUsingStack
     {
         static void Main(string[] args)
         {
-            QueueUsingStacks<int> myQueue = new QueueUsingStacks<int>();
+            //QueueUsingStacks<int> myQueue = new QueueUsingStacks<int>();
 
-            myQueue.Enqueue(10);
-            myQueue.Enqueue(20);
-            myQueue.Enqueue(30);
-            myQueue.PrintQueue();
+            //myQueue.Enqueue(10);
+            //myQueue.Enqueue(20);
+            //myQueue.Enqueue(30);
+            //myQueue.PrintQueue();
 
-            Console.WriteLine("Dequeue: " + myQueue.Dequeue()); // Output: Dequeue: 10
-            Console.WriteLine("Peek: " + myQueue.Peek());       // Output: Peek: 20
+            //Console.WriteLine("Dequeue: " + myQueue.Dequeue()); // Output: Dequeue: 10
+            //Console.WriteLine("Peek: " + myQueue.Peek());       // Output: Peek: 20
 
-            myQueue.Enqueue(40);
-            Console.WriteLine("Count: " + myQueue.Count());    // Output: Count: 3
+            //myQueue.Enqueue(40);
+            //Console.WriteLine("Count: " + myQueue.Count());    // Output: Count: 3
 
-            while (!myQueue.IsEmpty())
-            {
-                Console.WriteLine("Dequeue: " + myQueue.Dequeue());
-            }
+            //while (!myQueue.IsEmpty())
+            //{
+            //    Console.WriteLine("Dequeue: " + myQueue.Dequeue());
+            //}
+
+            QueueUsingSingleStack<int> q = new QueueUsingSingleStack<int>();
+
+            q.Enqueue(1);
+            q.Enqueue(3);
+            q.Enqueue(8);
+            q.Dequeue();
+            q.Dequeue();
+
+            Console.WriteLine("Peek: "+ q.Peek());
+            q.PrintQueue();
         }
     }
 }
---
+++ QueueUsingStack/QueueUsingStack/QueueUsingSingleStack.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueUsingStack
+{
+    class QueueUsingSingleStack<T>
+    {
+        private Stack<T> stack;
+
+        public QueueUsingSingleStack()
+        {
+            stack = new Stack<T>();
+        }
+
+        public void Enqueue(T item)
+        {
+            stack.Push(item);
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Queue is empty.");
+
+            // Base case: If the stack contains only one element, it is the oldest element in the queue
+            if (stack.Count == 1)
+                return stack.Pop();
+
+            // Recursive case: Pop an element from the stack and recursively call Dequeue
+            T temp = stack.Pop();
+            T dequeuedItem = Dequeue();
+
+            // Push the temporarily popped element back to the stack after recursion
+            stack.Push(temp);
+
+            return dequeuedItem;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Queue is empty.");
+
+            // Base case: If the stack contains only one element, it is the oldest element in the queue
+            if (stack.Count == 1)
+                return stack.Peek();
+
+            // Recursive case: Pop an element from the stack and recursively call Peek
+            T temp = stack.Pop();
+            T frontItem = Peek();
+
+            // Push the temporarily popped element back to the stack after recursion
+            stack.Push(temp);
+
+            return frontItem;
+        }
+
+        public bool IsEmpty()
+        {
+            return stack.Count == 0;
+        }
+
+        public int Count()
+        {
+            return stack.Count;
+        }
+        public void PrintQueue()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Queue is empty");
+            foreach(T item in stack.Reverse())
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
